c9e5a49aba0c9092e6a8f85de31deaa790b0a7f1
mostafa99hosny <m.hosney33@gmail.com>
2025-03-04 20:31:27 +0200

    edit in confige

--- config.php
+++ config.php
@@ -7,7 +7,6 @@ $password = '123456Mh*';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully!";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
